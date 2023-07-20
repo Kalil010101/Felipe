d6d3654b07bea3390b8799b2be5064a042c8e5a9
Kalil010101 <129905835+Kalil010101@users.noreply.github.com>
2023-07-20 18:21:22 -0300

    Add files via upload

---
+++ kalil-main/c36-corrida-para-mandar-pro-aluno-main/corrida multiplayer/js/Game.js
@@ -0,0 +1,21 @@
+class Game {
+  constructor() {}
+
+ getState (){
+  var gameStateRef = database.ref("gameState")
+  gameStateRef.on ("value",function (data){
+    gameState = data.val ()
+  })
+ }
+
+ update(state){
+  
+ }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount ()
+    form = new Form();
+    form.display();
+  }
+}
